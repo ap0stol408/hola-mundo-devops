9434c1a96ee6fcd89d6f9b2194400b7432a5788e
ap0stol <ap0stol408@proton.me>
2026-07-06 11:23:59 -0400

    testear  workflow

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hola mundo 2!");
+    res.send("Hola mundo desde AWS!");
 });
 
 app.listen(3000, () => {
